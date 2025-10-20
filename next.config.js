78dcc1de0b9ab29759bf7508086e16a4601d634e
joniko <jonatanflores8@gmail.com>
2025-10-20 01:01:55 -0300

    Fix: Remove Serwist from next.config to fix build error

--- next.config.js
+++ next.config.js
@@ -1,12 +1,4 @@
-const withSerwist = require('@serwist/next').default({
-  swSrc: 'src/sw.ts',
-  swDest: 'public/sw.js',
-  cacheOnNavigation: true,
-  reloadOnOnline: true,
-  disable: process.env.NODE_ENV === 'development',
-});
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-module.exports = withSerwist(nextConfig);
+module.exports = nextConfig;
